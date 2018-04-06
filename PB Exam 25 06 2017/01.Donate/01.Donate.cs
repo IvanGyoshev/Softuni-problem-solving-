bd07ca4c9b8550ede9d82256b05dc7894937819e
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-04-07 00:39:18 +0300

    Exam 06 2017

---
+++ PB Exam 25 06 2017/01.Donate/01.Donate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Donate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint days = uint.Parse(Console.ReadLine());
+            uint cookers = uint.Parse(Console.ReadLine());
+            uint cakes = uint.Parse(Console.ReadLine());
+            uint gofrets = uint.Parse(Console.ReadLine());
+            uint pancakes = uint.Parse(Console.ReadLine());
+            double donation = days * cookers*
+                (cakes*45d+gofrets*5.8d+pancakes*3.2d) * 7 / 8;
+            Console.WriteLine("{0:f2}", donation);
+        }
+    }
+}
