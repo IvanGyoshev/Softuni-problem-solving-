def408fb765c79e11114a73864a483a2634e6f46
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-04-08 02:06:49 +0300

    04.MoneyPrise

--- PB Exam 25 06 2017/02.WordSwimRecord/02.WordSwimRecord.cs
+++ PB Exam 25 06 2017/02.WordSwimRecord/02.WordSwimRecord.cs
@@ -14,9 +14,7 @@ namespace _02.WordSwimRecord
             double distance = double.Parse(Console.ReadLine());
             double timePerMeter = double.Parse(Console.ReadLine());
             double selfResult = timePerMeter * distance +
-                Math.Floor( distance / 15) * 12.5d;
-            Console.WriteLine("{0:f2}, {1:f2}, {2:f2}", selfResult,
-                timePerMeter * distance, Math.Floor(distance / 15) * 12.5d);
+                Math.Floor( distance / 15) * 12.5d;             
             if (selfResult < record)
                 Console.WriteLine("Yes, he succeeded! The new world record is " +
                     "{0:f2} seconds.", selfResult);
---
+++ PB Exam 25 06 2017/04.MoneyPrise/04.MoneyPrise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.MoneyPrise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint parts = uint.Parse(Console.ReadLine());
+            double moneyOfEveryPart = double.Parse(Console.ReadLine());
+            uint totalParts = 0u;
+            for (int i=1; i<=parts; i++)
+            {
+                uint input = uint.Parse(Console.ReadLine());
+                if (i % 2 == 0) totalParts += 2 * input;
+                else totalParts += input;
+            }
+            Console.WriteLine("The project prize was {0:f2} lv.",
+                totalParts* moneyOfEveryPart);
+        }
+    }
+}
