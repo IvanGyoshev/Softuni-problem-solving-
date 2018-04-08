da266c2db412f7d09f9dc4ce31cda8977fd902e4
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-04-08 03:35:51 +0300

    05.Triangle

---
+++ PB Exam 25 06 2017/05.Triangle/05.Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", new string('#', 4 * n + 1));
+            //top
+            for (int i = 0; i < n; i++)
+            {
+                if (i == n / 2 )
+                {
+                    Console.WriteLine("{0}{1}{2}(@){2}{1}{0}",
+                    new string('.', i + 1),
+                    new string('#', 2 * n - 1 - 2 * i),
+                    new string(' ', i - 1));
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{2}{1}{0}",
+                    new string('.', i + 1),
+                    new string('#', 2 * n - 1 - 2 * i),
+                    new string(' ', 2 * i + 1));
+                }
+            }                
+            //bottom
+            for (int i=0; i<n; i++)
+            Console.WriteLine("{0}{1}{0}", new string('.',n+1+i),
+                new string('#', 2*n - 1-2*i));
+        }
+    }
+}
