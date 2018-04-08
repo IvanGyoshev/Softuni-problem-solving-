30f327db8e1dfa3cef5ec2ad53464ba744f64271
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-04-09 01:10:19 +0300

    July 2017 exam 1 problem

---
+++ PB Exam 23 07 2017/July 2017/July 2017/01.Dance Hall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace July_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double aSide = double.Parse(Console.ReadLine());
+            double dancers = (height * width * 9000.0 - aSide * aSide * 10000.0d) / 7040;
+            Console.WriteLine(Math.Floor(dancers));
+        }
+    }
+}
