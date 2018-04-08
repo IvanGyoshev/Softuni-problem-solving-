b0266ec5d0fa326450a90f1bb74614a3a76bac4d
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-04-09 01:31:12 +0300

    problem 2 - decision

--- PB Exam 23 07 2017/July 2017/02.Choreography/02.Choreography.cs
+++ PB Exam 23 07 2017/July 2017/02.Choreography/02.Choreography.cs
@@ -10,6 +10,16 @@ namespace _02.Choreography
     {
         static void Main(string[] args)
         {
+            uint steps = uint.Parse(Console.ReadLine());
+            uint dancers = uint.Parse(Console.ReadLine());
+            uint days = uint.Parse(Console.ReadLine());
+            double stepsPerDay = (100d*steps / days) / steps * 1.0d;            
+            double stepsPersentPerDancer = Math.Ceiling(stepsPerDay) / dancers * 1.0d;
+            if (stepsPerDay <= 13)
+                Console.WriteLine("Yes, they will succeed in that goal! {0:f2}%.",
+                    stepsPersentPerDancer);
+            else Console.WriteLine("No, They will not succeed in that goal! Required {0:f2}% steps to be learned per day.",
+                    stepsPersentPerDancer);
         }
     }
 }
