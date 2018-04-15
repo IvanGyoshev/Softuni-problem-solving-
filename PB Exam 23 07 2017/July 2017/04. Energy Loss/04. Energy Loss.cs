ee6d48aec5ed357ea1bbe5b276fe6d13291482aa
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-04-15 08:56:18 +0300

    04. Energy Loss Изпит по "Програмиране за начинаещи" - 23 юли 2017 Задача 4. Разход на енергия След финалите всички танцьори се връщат към регулярните си тренировки в залата. Всички тренират определен брой дни по определени часове. Да се изчисли колко средно енергия  е останала неизхабена за всички тренировъчни дни като се има предвид че:
    	Проценти изхабена енергия на човек
    Четна тренировка и четни часове	68%
    Нечетна тренировка и четни часове	49%
    Четна тренировка и нечетни часове	65%
    Нечетна тренировка и нечетни часове	30%
    
    Всеки танцьор започва със 100% енергия.
    Вход
    От конзолата се четат поредица от числа, всяко на отделен ред:
    1.	N – брой тренировъчни дни – цяло число в интервала [0 … 31].
    2.	А – брой танцьори – цяло число в интервала  [0 … 100].
    За всеки тренировъчен ден се прочита на отделен ред:
    •	Часовете, които са тренирали – цяло число в интервала [0 … 12].
    Изход
    Отпечатва се 1 ред в следния формат:
    АКО средния разход на енергия за тренировъчните дни е 50% или по - малко
    •	“They feel good! Energy left: {останала енергия}”.
    AКО средния разход на енергия за тренировъчните дни  е над 50%
    •	“They are wasted! Energy left: {останала енергия}”.
    
    Резултатът трябва да се форматира до втория знак след десетичната запетая.

---
+++ PB Exam 23 07 2017/July 2017/04. Energy Loss/04. Energy Loss.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Energy_Loss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort nDays = ushort.Parse(Console.ReadLine());
+            ushort dancers = ushort.Parse(Console.ReadLine());
+            ushort hours = 0;
+            double energyCapacity = 100d * dancers * nDays ;
+            double wastedEnergy = 0.00d;
+            for (int i=1; i<=nDays; i++)
+            {
+                hours = ushort.Parse(Console.ReadLine());
+                if (i % 2 != 0 && hours % 2 != 0) wastedEnergy += dancers * 30;
+                else if (i % 2 != 0 && hours % 2 == 0) wastedEnergy += dancers * 49;
+                else if (i % 2 == 0 && hours % 2 == 0) wastedEnergy += dancers * 68;
+                else if (i % 2 == 0 && hours % 2 != 0) wastedEnergy += dancers * 65;
+            }
+            double result = (energyCapacity - wastedEnergy) / nDays / dancers;
+            if (result > 50)
+                Console.WriteLine("They feel good! Energy left: {0:f2}",result);
+            else if (result <= 50)
+                Console.WriteLine("They are wasted! Energy left: {0:f2}", result);
+
+        }
+    }
+}
