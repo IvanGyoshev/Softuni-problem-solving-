6c2520470ae9ab6bdcdd22b79cd49d2daa603928
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-04-15 08:31:33 +0300

    03. Final Competition Изпит по "Програмиране за начинаещи" - 23 юли 2017 Задача 3. Финален конкурс След успешно класиране групата заминава за  финалното състезание. След представяне всяка група получава парична награда. Тя зависи от това, дали финалът е в България или в чужбина, от броя точки, които журито е дало и сезона през който се е провело състезанието. •	Ако състезанието се е провело в България паричната награда се изчислява като се умножат точките по броя участниците. •	Ако се е провело в чужбина – се умножават броя участници по броя точки и към тях се добавя 50% от получената сума. От получената сума се изваждат разходите посочени в проценти спрямо сезона който се е провел : 	Лято	Зима България	5%	8% Чужбина	10%	15%
    След завръщането си групата дарява 75% от сумата, след приспаднатите разходи, за благотворителност, а останалата сума се разпределя между членовете на групата.
    Да се  напише програма, която изчислява колко пари са дадени за благотворителност и колко е получил всеки един член на групата.
    
    Вход
    От конзолата се четат 4 реда:
    1.	Брой танцьори – цяло число в интервала [1…10];
    2.	Брой точки – реално число в интервала [1.00…10000.00];
    3.	Сезон – ”summer“ / “winter“;
    4.	Място – „Bulgaria“ / „Abroad“;
    Изход
    На конзолата се отпечатват 2 реда:
    •	Сумата, която са дали за благотворителност, форматирана до втория знак след десетичната запетая
    „Charity - {обща сума}“
    •	Сумата, която е получил един танцьор, форматирана до втория знак след десетичната запетая
    „Money per dancer - {сума}“

---
+++ PB Exam 23 07 2017/July 2017/03. Final Competition/03. Final Competition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Final_Competition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte dancers = sbyte.Parse(Console.ReadLine());
+            double points = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string place = Console.ReadLine();
+            if (place=="Bulgaria")
+            {
+                switch (season)
+                {
+                    case "summer": points = points * dancers * 0.95d; break;
+                    case "winter": points = points * dancers * 0.92d; break;
+                }
+            }
+            else if (place=="Abroad")
+            {
+                switch (season)
+                {
+                    case "summer": points = points * dancers *1.5* 0.9d; break;
+                    case "winter": points = points * dancers * 1.5*0.85d; break;
+                }
+            }
+            double charity = 0.75d*points;
+            double moneyPerDancer = 0.25d*points/dancers;
+            Console.WriteLine("Charity - {0:f2}", charity);
+            Console.WriteLine("Money per dancer - {0:f2}", moneyPerDancer);
+        }
+    }
+}
