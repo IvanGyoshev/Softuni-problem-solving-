71325e98ea059087b7404080e89405f18dbb5ad0
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-11 22:57:05 +0300

    Задача 1. Шивашки цех Шивашки цех приема поръчки за ушиване на покривки и карета за маси за заведения. Покривките са правоъгълни, каретата са квадратни, броят им винаги е еднакъв. Покривката трябва да виси с 30 см от всеки ръб на масата. Страната на каретата е половината от дължината на масите. Във всяка поръчка се включва информация за броя и размерите на масите. Напишете програма, която пресмята цената на поръчка в долари и в левове, като квадратен метър плат за правоъгълна покривка струва 7 долара, а за каре – 9 долара. Курсът на долара е 1.85 лева. Вход Потребителят въвежда 3 числа, по едно на ред: 1.	Брой правогълни маси – цяло число в интервала [0..500]; 2.	Дължина на правоъгълните маси в метри – реално число в интервала [0.00...3.00]; 3.	Широчина на правоъгълните маси в метри – реално число в интервала [0.00...3.00]; Изход Да се отпечатат на конзолата две числа: цената на изделията в долари и в левове. o	"{цена в долари} USD" o	"{цена в левове} BGN" Резултатите да се закръглят до два знака след  десетичната запетая.

---
+++ Exam 03 09 2017/01. Tailoring Workshop/Tailoring Workshop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Tailoring_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint tables = uint.Parse(Console.ReadLine());
+            double aSideOfTable = double.Parse(Console.ReadLine());
+            double bSideOfTable = double.Parse(Console.ReadLine());
+            double aSideOfCover = aSideOfTable + 2*0.3d;
+            double bSideOfCover = bSideOfTable + 2*0.3d;
+            double sideOfKare = aSideOfTable /2.0d;
+            double bill = tables * (aSideOfCover*bSideOfCover*7.00d+
+                sideOfKare*sideOfKare*9.00d)*1.0d;
+            Console.WriteLine("{0:f2} USD", bill);
+            Console.WriteLine("{0:f2} BGN", bill*1.85d);
+
+        }
+    }
+}
