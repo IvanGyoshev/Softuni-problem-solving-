4154efe85de344f837a3670273c0dc967041f564
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-11 20:12:06 +0300

    Изпит по "Програмиране за начинаещи" - 23 юли 2017 Задача 5. Купа Като най-запаления фен на групата по танци решаваш да им направиш най-милия и незабравим подарък, който един кандидат - програмист може да сътвори, а именно – „конзолна купа“. Купата е широка 5*N. Вход Входът се чете от конзолата и съдържа само едно число: •	N четно число в интервала[6…100] Изход Да се отпечата на конзолата купа като в примерите по-долу.

---
+++ PB Exam 23 07 2017/July 2017/05.Cup/05.Cup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Cup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort n = ushort.Parse(Console.ReadLine());
+            for (int i = 0; i < n / 2; i++)
+                Console.WriteLine("{0}{1}{0}", new string('.', n + i),
+                    new string('#', 3 * n - 2 * i));
+            for (int i = 0; i < n / 2+1; i++)
+                Console.WriteLine("{0}#{1}#{0}", new string('.', 3*n/2 + i),
+                    new string('.', 2 * n -2- 2 * i));
+            Console.WriteLine("{0}{1}{0}", new string('.', 2 * n),
+                new string('#', n));
+            for (int i = 0; i < n / 2; i++)
+                Console.WriteLine("{0}{1}{0}", new string('.',2* n -2),
+                    new string('#', n + 4));
+            Console.WriteLine("{0}D^A^N^C^E^{0}", new string ('.',(5*n-10)/2));
+            for (int i = 0; i < n / 2 + 1; i++)
+                Console.WriteLine("{0}{1}{0}", new string('.', 2 * n - 2),
+                    new string('#', n + 4));
+        }
+    }
+}
