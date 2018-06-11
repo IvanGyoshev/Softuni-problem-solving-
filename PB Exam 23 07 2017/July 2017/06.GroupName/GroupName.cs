7c425749056dc8340064cdb687b4a6ae56d1d810
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-11 20:13:14 +0300

    Изпит по "Програмиране за начинаещи" - 23 юли 2017 Задача 6. Име на група Групата сменя своя ръководител и съответно трябва да се избере ново име. За целта всеки един танцьор дава предложения. Предложенията винаги се дават по азбучен ред от първата буква в азбуката до въведена. Името на групата съдържа и цифра. Винаги цифрата започва от нула и достига до въведената. Имената се дават по азбучен ред. Да се изчисли колко предложения са били дадени преди да се достигне до името на групата, като се има предвид, че дължината на името винаги е 5 символа и винаги първия символ е главна буква, последващите 3 символа са малки букви ,а  последния е цифра. Вход От конзолата се четат 5 реда: •	Голяма буква – символ в интервала [A - Z] •	Малка буква – символ в интервала [a - z] •	Малка буква – символ в интервала [a - z] •	Малка буква – символ в интервала [a - z] •	Цифра – цяло число в интервала [0 - 9] Изход Да се отпечата на конзолата цяло число – броя предложения точно преди достигането на името.
    вход	изход	Обяснения
    A
    a
    a
    a
    0	0
    K
    a
    b
    c
    5
    	395	Aaaa0 Aaaa1 Aaaa2 Aaaa3 Aaaa4 Aaaa5 Aaab0 Aaab1 Aaab2 Aaab3 Aaab4 Aaab5 Aaac0 Aaac1 Aaac2 Aaac3 Aaac4 Aaac5 Aaba0 Aaba1 Aaba2 Aaba3 Aaba4 Aaba5 Aabb0 Aabb1 Aabb2 Aabb3 Aabb4 Aabb5 Aabc0 Aabc1 Aabc2 Aabc3 Aabc4 Aabc5 Baaa0 Baaa1 Baaa2 Baaa3 Baaa4 Baaa5 Baab0 Baab1 Baab2 Baab3 Baab4 Baab5 Baac0 Baac1 Baac2 Baac3 Baac4 Baac5 Baba0 Baba1 Baba2 Baba3 Baba4 Baba5 Babb0 Babb1 Babb2 Babb3 Babb4 Babb5 Babc0
    ...
    
    Kaac2 Kaac3 Kaac4 Kaac5 Kaba0 Kaba1 Kaba2 Kaba3 Kaba4 Kaba5 Kabb0 Kabb1 Kabb2 Kabb3 Kabb4 Kabb5 Kabc0 Kabc1 Kabc2 Kabc3 Kabc4
    I
    v
    a
    n
    1	5543	Aaaa0 Aaaa1 Aaab0 Aaab1 Aaac0 Aaac1 Aaad0 Aaad1 Aaae0 Aaae1 Aaaf0 Aaaf1 Aaag0 Aaag1 Aaah0 Aaah1 Aaai0 Aaai1 Aaaj0 Aaaj1 Aaak0 Aaak1 Aaal0 Aaal1 Aaam0 Aaam1 Aaan0 Aaan1 Abaa0 Abaa1 Abab0 Abab1 Abac0 Abac1 Abad0 Abad1 Abae0 Abae1 Abaf0 Abaf1 Abag0 Abag1 Abah0 Abah1 Abai0 Abai1 Abaj0 Abaj1 Abak0 Abak1 Abal0 Abal1 Abam0 Abam1 Aban0 Aban1 Acaa0 Acaa1 Acab0 Acab1 Acac0 Acac1 Acad0 Acad1 Acae0 Acae1 Acaf0 Acaf1 Acag0 Acag1 Acah0 Acah1 Acai0 Acai1 Acaj0 Acaj1 Acak0 Acak1 Acal0 Acal1
    ...
    
    Ivam0 Ivam1 Ivan0

---
+++ PB Exam 23 07 2017/July 2017/06.GroupName/GroupName.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.GroupName
+{
+    class GroupName
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = Char.Parse(Console.ReadLine());
+            char secondLetter = Char.Parse(Console.ReadLine());
+            char thirdLetter = Char.Parse(Console.ReadLine());
+            char fourthLetter = Char.Parse(Console.ReadLine());
+            sbyte lastFigure = SByte.Parse(Console.ReadLine());
+            int counter = 0;
+            for (char i='A'; i<=firstLetter; i++)
+                for (char j = 'a'; j <= secondLetter; j++)
+                    for (char k = 'a'; k <= thirdLetter; k++)
+                        for (char l = 'a'; l <= fourthLetter; l++)
+                            for (sbyte m = 0; m <= lastFigure; m++)
+                            {
+                                  //Console.Write("{0}{1}{2}{3}{4} ",
+                                   // i, j, k, l, m);
+                                counter++;
+                            }
+            Console.WriteLine("{0}", counter-1);
+        }
+    }
+}
