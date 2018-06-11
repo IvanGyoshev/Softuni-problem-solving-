bc83f256be174ad4474aba2531f1ba0a1a1ded3f
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-11 23:48:54 +0300

    Задача 2. Стипендии Учениците могат да кандидатстват за социална стипендия или за стипендия за отличен успех. Изискване за социална стипендия – доход на член от семейството по-малък от минималната работна заплата и успех над 4.5. Размер на социалната стипендия – 35% от минималната работна заплата. Изискване за стипендия за отличен успех – успех над 5.5, включително. Размер на стипендията за отличен успех – успехът на ученика, умножен по коефициент 25. Напишете програма, която при въведени доход, успех и минимална работна заплата, дава информация дали ученик има право да получава стипендия, и стойността на стипендията, която е по-висока за него. Вход Потребителят въвежда 3 числа, по едно на ред: 1.	Доход в лева – реално число в интервала [0.00..6000.00]; 2.	Среден успех –  реално число в интервала [2.00...6.00]; 3.	Минимална работна заплата – реално число в интервала [0.00..1000.00]; Изход •	Ако ученикът няма право да получава стипендия, се извежда: "You cannot get a scholarship!" •	Ако ученикът има право да получава социална стипендия и тя е по-висока от стипендията за отличен успех: "You get a Social scholarship {стойност на стипендия} BGN" •	Ако ученикът има право да получава стипендия за отличен успех и тя е по-висока или равна по стойност на социалната стипендия за него: "You get a scholarship for excellent results {стойност на стипендията} BGN" Резултатът се закръгля до по-малкото цяло число.

---
+++ Exam 03 09 2017/02. Scholarship/Scholarship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Scholarship
+{
+    class Scholarship
+    {
+        static void Main(string[] args)
+        {
+            double incomePerPerson = double.Parse(Console.ReadLine());
+            double gradeStatus = double.Parse(Console.ReadLine());
+            double minimalSalary = double.Parse(Console.ReadLine());
+            double socialBonus = 0.35*minimalSalary;
+            double excellentBonus = gradeStatus *25.0d;
+            if (incomePerPerson < minimalSalary && gradeStatus > 4.5 && gradeStatus<5.5)              
+                Console.WriteLine("You get a Social scholarship {0} BGN",
+                     Math.Floor(socialBonus));           
+            else if (gradeStatus >= 5.5 && socialBonus <= excellentBonus)
+                Console.WriteLine("You get a scholarship for excellent results {0} BGN"
+                    , Math.Floor(excellentBonus));
+            else if (gradeStatus >= 5.5 && socialBonus > excellentBonus)
+                Console.WriteLine("You get a scholarship for excellent results {0} BGN"
+                    , Math.Floor(excellentBonus));
+            else 
+                Console.WriteLine("You cannot get a scholarship!");
+        }
+    }
+}
