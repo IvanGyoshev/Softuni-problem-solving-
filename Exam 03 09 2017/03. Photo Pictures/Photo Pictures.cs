9b361e1eb04e4f396da97eab09afefbcb74180f4
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-13 23:14:29 +0300

    Задача 3. Фото снимки Фото-ателие приема поръчки за изваждане на снимки на хартия в няколко размера със следните цени за една снимка. Една поръчка включва снимки само от един размер. В зависимост от поръчания брой снимки, се прави различна отстъпка. Размер	Единична цена	Отстъпка от цената 9X13	0.16 лв.	Над 50 броя включително – 5% 10X15	0.16 лв.	Над 80 броя включително – 3% 13X18	0.38 лв.	От 50 до 100 броя включително – 3% Над 100 броя – 5% 20X30	2.90 лв.	От 10 до 50 броя включително – 7% Над 50 броя – 9%
    Ако поръчката се направи онлайн, се ползва допълнителна отстъпка 2%, независимо от броя на снимките.
    Вход
    Потребителят въвежда 3 реда:
    1.	Брой снимки – цяло число в интервала [0..1000];
    2.	Вид снимки – текст "9X13" или "10X15" или "13X18" или "20X30";
    3.	Начин на поръчване – текст "online" или "office";
    Изход
    Извежда се едно число – стойността на поръчката, в следния формат:
    o	"{стойност на поръчката}BGN"
    Резултатът да се форматира до два знака след десетичната запетая.
    Примерен вход и изход
    Вход	Изход	Обяснения
    5
    9X13
    online	0.78BGN	Цената е: 5 броя * 0.16 лв. = 0.80 лв. Не се ползва отстъпка за брой снимки, защото 5 < 50. Тъй като начинът на поръчване е online се ползва 2% отстъпка, 0.80 * 0.98 = 0.784 лв. Числото се форматира до 2 знака – 0.78
    105
    20X30
    office	277.10BGN	Цената е: 105 броя * 2.90 лв. = 304.50 лв.
    105 > 50 → отстъпката за брой снимки е 9%. Цената става 304.50 * 0.91 = 277.095 лв.
    Тъй като начинът на поръчване е office, не се ползва допълнителна отстъпка. Числото се форматира до 2 знака – 277.10

---
+++ Exam 03 09 2017/03. Photo Pictures/Photo Pictures.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Photo_Pictures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint picturesCount = uint.Parse(Console.ReadLine());
+            string size = Console.ReadLine();
+            string typeOfOrder = Console.ReadLine();
+            double bill = 0.00d;
+            switch(size)
+            {
+                case "9X13":
+                    bill = picturesCount * 0.16d;
+                    if (picturesCount > 50) bill *= 0.95d;
+                    break;
+                case "10X15":
+                    bill = picturesCount * 0.16d;
+                    if (picturesCount > 80) bill *= 0.97d;
+                    break;
+                case "13X18":
+                    bill = picturesCount * 0.38d;
+                    if (picturesCount > 50 && picturesCount<=100) bill *= 0.97d;
+                    else if (picturesCount > 100) bill *= 0.95d;
+                    break;
+                case "20X30":
+                    bill = picturesCount * 2.90d;
+                    if (picturesCount > 10 && picturesCount <= 50) bill *= 0.93d;
+                    else if (picturesCount > 50) bill *= 0.91d;
+                    break;
+            }
+            if (typeOfOrder == "online") bill *= 0.98d;             
+            Console.WriteLine("{0:f2}BGN", bill);
+        }
+    }
+}
