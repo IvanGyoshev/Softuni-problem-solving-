52c2866b0de9e5bdcc20bba3bd35d31ca0685e41
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-14 00:08:25 +0300

    Задача 5. Снежинка Напишете програма която по зададено число N разпечатва на конзолата снежинка. Височината на фигурата е 2 * n + 1. Широчината на фигурата е 2 * n + 3. Вход От конзолата се чете 1 число: N Ограничения: 3 ≤ N ≤ 100. Изход Да се отпечата на конзолата снежинка. Примерен вход и изход Вход	Изход 3	*...*...* .*..*..*. ..*****.. ********* ..*****.. .*..*..*. *...*...* 5	*.....*.....* .*....*....*. ..*...*...*.. ...*..*..*... ....*****.... ************* ....*****.... ...*..*..*... ..*...*...*.. .*....*....*. *.....*.....* 8	*........*........* .*.......*.......*. ..*......*......*.. ...*.....*.....*... ....*....*....*.... .....*...*...*..... ......*..*..*...... .......*****....... ******************* .......*****....... ......*..*..*...... .....*...*...*..... ....*....*....*.... ...*.....*.....*... ..*......*......*.. .*.......*.......*. *........*........*

---
+++ Exam 03 09 2017/05. Snowflake/Snowflake.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Snowflake
+{
+    class Snowflake
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //top
+            for (int i = 0; i < n - 1; i++)
+                Console.WriteLine("{0}*{1}*{1}*{0}",
+                    new string('.', i), new string('.', n-i));
+            //middle part 1st line uppon
+            Console.WriteLine("{0}{1}{0}",
+                new string('.', n-1), new string ('*', 5));
+            //middle part - center
+            Console.WriteLine(new string('*', 2 * n + 3));
+            //middle part 1st line downstairs
+            Console.WriteLine("{0}{1}{0}",
+                new string('.', n - 1), new string('*', 5));
+            //bottom
+            for (int i = n - 1; i >0 ; i--)
+                Console.WriteLine("{0}*{1}*{1}*{0}",
+                    new string('.', i-1), new string('.', n - i+1));
+        }
+    }
+}
