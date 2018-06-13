b32c9d29d44230610f8396c7ebee6521687bfa3a
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-14 00:31:46 +0300

    Задача 6. Банкноти и монети Имаме банкноти и монети по 1лв., по 2лв. и по 5лв. Да се напише програма, която прочита въведените от потребителя брой банкноти и монети и сума, и извежда на екран всички възможни начини по които сумата може да се изплати с наличните банкноти. Вход Входът се чете от конзолата и съдържа точно 4 реда: 1.	Брой монети по 1лв. - цяло положително число; 2.	Брой монети по 2лв. - цяло положително число; 3.	Брой банкноти по 5лв. - цяло положително число; 4.	Сума - цяло положително число в интервала [1…1000]; Изход Да се отпечатат на конзолата всички комбинации от дадените номинали, образуващи сумата, форматирани по следния начин: o	"{бр. 1лв.} * 1 lv. + {бр. 2лв.} * 2 lv. + {бр. 5лв.} * 5 lv. = {сума} lv." Примерен вход и изход Вход	Изход 3 2 3 10 	0 * 1 lv. + 0 * 2 lv. + 2 * 5 lv. = 10 lv. 1 * 1 lv. + 2 * 2 lv. + 1 * 5 lv. = 10 lv. 3 * 1 lv. + 1 * 2 lv. + 1 * 5 lv. = 10 lv. Вход	Изход 5 3 1 7 	0 * 1 lv. + 1 * 2 lv. + 1 * 5 lv. = 7 lv. 1 * 1 lv. + 3 * 2 lv. + 0 * 5 lv. = 7 lv. 2 * 1 lv. + 0 * 2 lv. + 1 * 5 lv. = 7 lv. 3 * 1 lv. + 2 * 2 lv. + 0 * 5 lv. = 7 lv. 5 * 1 lv. + 1 * 2 lv. + 0 * 5 lv. = 7 lv.

---
+++ Exam 03 09 2017/06. Profit/Profit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Profit
+{
+    class Profit
+    {
+        static void Main(string[] args)
+        {
+            uint oneCoins = uint.Parse(Console.ReadLine());
+            uint twoCoins = uint.Parse(Console.ReadLine());
+            uint fiveCoins = uint.Parse(Console.ReadLine());
+            uint sum = uint.Parse(Console.ReadLine());
+            for (uint i=0; i<=oneCoins; i++)
+            {
+                
+                for (uint j=0; j<=twoCoins ; j++)
+                {
+                    for (uint k = 0; k <= fiveCoins; k++)
+                    {
+                        if (sum == 1 * i + 2 * j + 5 * k)
+                            Console.WriteLine("{0} * 1 lv. + {1} * 2 lv. + {2} * 5 lv. = {3} lv.",
+                                i, j, k, sum);
+                    }
+                }
+            }
+
+        }
+    }
+}
