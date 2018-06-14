c3ee996f5637c61857faaf15b7ab5d3945135e45
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-14 22:50:32 +0300

    Задача 1. Рожден ден За рождения си ден Любомир получил аквариум с формата на паралелепипед. Първоначално прочитаме от конзолата на отделни редове размерите му – дължина, широчина и височина в сантиметри. Трябва да се пресметне колко литра вода ще събира аквариума, ако се знае, че определен процент от вместимостта му е заета от пясък, растения, нагревател и помпа. Един литър вода се равнява на един кубичен дециметър/ 1л=1 дм3/. Да се напише програма, която изчислява литрите вода, която са необходими за напълването на аквариума. Вход От конзолата се четат 4 реда: 1.	Дължина в см – цяло число в интервала [10 … 500]; 2.	Широчина в см – цяло число в интервала [10 … 300]; 3.	Височина в см – цяло число в интервала [10… 200]; 4.	Процент  – реално число в интервала [0.000 … 100.000]; Изход Да се отпечата на конзолата едно число: •	литрите вода, които ще  събира аквариума, форматирани до третия знак след десетичната запетая. Примерен вход и изход Вход	Изход	Обяснения 85 75 47 17	248.689	Изчисляваме обем на аквариум: обем на аквариум= 85*75*47=299625 см3 общо литри, които ще събере: 299625 * 0.001=299.625 литра процент: 17*0.01=0.17 литрите, които реално ще трябват : 299.625*(1-0.17) = 248.68875 литра Вход	Изход 105 77 89 18.5	586.445

---
+++ Exam - 17 September 2017/01.Birthday/Birthday.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Birthday
+{
+    class Birthday
+    {
+        static void Main(string[] args)
+        {
+            uint length = uint.Parse(Console.ReadLine());
+            uint width = uint.Parse(Console.ReadLine());
+            uint height = uint.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            double volume = (1-percent/100.00d)*length*width*height/1000.0d;
+            Console.WriteLine("{0:f3}", volume);
+
+        }
+    }
+}
