2b02692b3feaf194615269b393572922fd7341fe
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-16 10:47:53 +0300

    Задача 4. Дядо Ставри Дядо Ставри има казан за ракия и всеки ден вари от любимата си напитка. Ако вари N дена и всеки ден получава различно количество ракия и алкохолен градус, да се намери за всичките дни общото количество ракия и градуса на получената смес. Вход От конзолата се четат поредица от числа, всяко на отделен ред: •	На първия ред – N – брой дни  – цяло число в интервала [1...300] •	За всеки един ден на отделен ред : 	количество на ракията – реално число в интервала [1.00...500.00] 	градус на получената напитка - реално число в интервала [10.00...99.00] Изход Да се отпечатат на конзолата 3 реда, както следва: •	Първи ред – да се отпечатва съобщение форматирано до втори знак „Liter:{общо литрите}“ •	Втори ред – да се отпечатва съобщение форматирано до втори знак „Degrees:{градусите на общата смес}  ; •	Трети ред – да се отпечатва: 	Ако градусът е по-малко от 38, да се отпечата съобщение „Не е добре, трябва преваряване!“. "Not good, you should baking!". 	Ако е от 38 до 42, да се отпечата съобщение „Супер!“ "Super!" 	Ако е повече от 42, да се отпечата „Разреждане с дестилирана вода!“ "Dilution with distilled water!" Примерен вход и изход Вход	Изход	Обяснения	Вход	Изход 3 100 45 50 55 150 36 	Liter: 300.00 Degrees: 42.17 Dilution with distilled water!	Първи ден: общото=100 литра      100 * 45=4500 Втори ден: общото= 100+50=150;     4500+55*50=7250; Трети ден: общото= 150+150=300;     7250+150*36=12650; градусите=12650 / 300=42.166666…	2 200 43 200 40 	Liter: 400.00 Degrees: 41.50 Super!

---
+++ Exam - 17 September 2017/04.Grandpa Stavri/Grandpa Stavri.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Grandpa_Stavri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint nDays = uint.Parse(Console.ReadLine());
+            double yeniRaki = 0.00d;
+            double grades = 0.00d;
+            double volume = 0.00d;
+            double sumGrades = 0.00d;
+            for (int i=0; i< nDays; i++)
+            {
+                yeniRaki = double.Parse(Console.ReadLine());
+                grades = double.Parse(Console.ReadLine());
+                volume += yeniRaki;
+                sumGrades += yeniRaki * grades;
+            }
+            Console.WriteLine("Litter:{0:f2}", volume);
+            Console.WriteLine("Degrees:{0:f2}", sumGrades/volume);
+            if (sumGrades/volume<38)
+            Console.WriteLine("Not good, you should baking!");
+            else if (sumGrades / volume >= 38 && sumGrades / volume <42)
+                Console.WriteLine("Super!");
+            else if (sumGrades / volume >42)
+                Console.WriteLine("Dilution with distilled water!");
+        }
+    }
+}
