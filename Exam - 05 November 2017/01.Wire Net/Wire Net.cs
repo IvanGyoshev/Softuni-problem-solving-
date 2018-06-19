5bd71d59bb2d7555a993f23d6e0a0307559cf14e
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-19 19:24:39 +0300

    Задача 1. Оградна мрежа Един човек има правоъгълен двор, който иска да огради с мрежа. Напишете програма, която пресмята колко: •	линейни метра мрежа ще му е нужна, за да огради двора •	ще струва това при дадена цена на линеен метър •	ще тежи закупената мрежа при дадена височина и тегло на квадратен метър
    Вход
    От конзолата се четат 5 числа:
    1.	Дължината в метри на мястото - цяло число в интервала [10 ... 150];
    2.	Широчината в метри на мястото - цяло число в интервала [10 ... 150];
    3.	Височината на мрежата в метри - реално число в интервала [0.50 ... 3.50];
    4.	Цената на един линеен метър от мрежата в лева - реално число в интервала [0.50 ... 10.00];
    5.	Теглото на един квадратен метър от мрежата в килограм - реално число в интервала [0.50 ... 5.00].
    Изход
    На конзолата да се отпечатат 3 числа:
    1.	Дължината в метри на мрежата, която трябва да бъде закупена;
    2.	Цената в лева на мрежата, която трябва да бъде закупена, закръглена до втория знак;
    3.	Теглото на цялата мрежа, която трябва да бъде закупена, закръглено до третия знак.
     
    Примерен вход и изход
    Вход	Изход	Обяснения
    10
    50
    1.5
    2.10
    0.95	120
    252.00
    171.000	Дължина на мрежата = 2 * 10 + 2 * 50 = 120 метра
    Цена на мрежата = 120 * 2.10 = 252.00 лева
    Площта на мрежата = 120 * 1.5 = 180 квадратни метра
    Теглото на мрежата = 180 * 0.95 = 171.000 килограма
    Вход	Изход	Вход	Изход
    50
    75
    2.0
    3.50
    1.55	250
    875.00
    775.000
    	10
    100
    1
    1.99
    0.99	220
    437.80
    217.800

---
+++ Exam - 05 November 2017/01.Wire Net/Wire Net.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Wire_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfPlace = int.Parse(Console.ReadLine());
+            int widthOfPlace = int.Parse(Console.ReadLine());
+            double heightOfNet = double.Parse(Console.ReadLine());
+            double priceOfNetPerMeter = double.Parse(Console.ReadLine());
+            double weightOfNetPerMeter = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}\n{1:f2}\n{2:f3}", 2 * (lengthOfPlace + widthOfPlace),
+                2 * (lengthOfPlace + widthOfPlace)*priceOfNetPerMeter,
+               2 * (lengthOfPlace + widthOfPlace) * heightOfNet * weightOfNetPerMeter );
+            
+            
+        }
+    }
+}
---
+++ Exam - 05 November 2017/05.Java/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Java
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i=1; i<=n; i++)
+            Console.WriteLine("{0}~ ~ ~ {1}", new string(' ', n), new string(' ', 2 * n));
+
+            Console.WriteLine("{0} ", new string('=', 3*n+5));
+
+            //Console.WriteLine("{0} ", new string('=', 3 * n + 5));
+
+            Console.WriteLine("{0}   ", new string('=', 3 * n));
+
+        }
+    }
+}
