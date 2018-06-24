a3341499b5618b735837326f0582e5a14d3b6450
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-06-24 16:33:49 +0300

    Изпит по "Основи на програмирането" – 5 Ноември 2017 Задача 3. Куриер Експрес Куриерска фирма доставя пратки в цялата страна. За услуга тип "standard", срокът за доставка е 3 работни дни и фирмата калкулира цените при следните условия: •	За пратки по - леки от 1000 гр. – 3 стотинки на километър. •	От 1 кг до 10  кг – 5 стотинки на километър. •	От 11 кг до 40 кг – 10 стотинки на километър. •	От 41 кг до 90  кг – 15 стотинки на километър. •	От 91 кг до 150 кг – 20 стотинки на километър. Ако клиентите желаят експересна доставка (услуга "express"), фирмата извършва услугата в рамките на 24 часа, като начислява надценка за всеки километър както следва: •	За пратки по - леки от 1 кг. – на килограм по 80 % от съответната цена на километър; •	От 1 кг до 10  кг – на килограм по 40 % от съответната цена на километър; •	От 11 кг до 40 кг – на килограм по 5 % от съответната цена на километър; •	От 41 кг до 90  кг – на килограм по 2 % от съответната цена на километър; •	От 91 кг до 150 кг – на килограм по 1 % от съответната цена на километър; Напишете програма, която да пресмята при зададено разстояние в км. , тегло на пратката и вида услуга, каква ще бъде стойността за доставка на дадена пратка. Вход Входът се чете от конзолата и съдържа 3 реда: 1.	Тегло на пратката в килограми – реално число в интервала [0.01 ... 150.00]; 2.	Тип услуга – "standard" или "express"; 3.	Разстоянието в километри – цяло число в интервала [1 ... 1000] Изход Да се отпечата на конзолата един ред: "The delivery of your shipment with weight of {тегло} kg. would cost {цена} lv."
    •	Теглото да бъде закръглено до третия знак след десетичния разделител
    •	Цената да бъде закръглена до втория знак след десетичния разделител
    
    Примерен вход и изход
    Вход	Изход	Вход	Изход
    1.5
    standard
    100		The delivery of your shipment with weight of 1.500 kg. would cost 5.00 lv.	87
    express
    130	The delivery of your shipment with weight of 87.000 kg. would cost 53.43 lv.
    Обяснения	Обяснения
    При услуга "standard" за пратка с тегло 1.5 кг. транспортът на километър е 0.05 лв.
    100 км. по 5 стотинки = 5.00 лв. 	За пратка с тегло 87 кг., транспортът на километър е 0.15 лв.
    130 км. по 15 стотинки = 19.50 лв.
    Надценка за услуга „express”:
                          2% от 0.15 лв. = 0.003 лв.
                          87 кг. по 0.003 лв. = 0.261 лв.
                          130 км. по 0.261 лв. = 33.93 лв.
    Обща стойност: 19.50 лв. + 33.93 лв. = 53.43 лв.

---
+++ Exam - 05 November 2017/03.Courier Express/Courier Express.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Courier_Express
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = double.Parse(Console.ReadLine());
+            string service = Console.ReadLine();
+            int distance = int.Parse(Console.ReadLine());
+            double costs = 0.0d;
+            if (service=="standard")
+            {
+                if (weight < 1) costs = 0.03d;
+                else if (weight >= 1 && weight <= 10) costs = 0.05d;
+                else if (weight >= 11 && weight <= 40) costs = 0.1d;
+                else if (weight >= 41 && weight <= 90) costs = 0.15d;
+                else if (weight >= 91 && weight <= 150) costs = 0.2d;
+            }
+            else if (service == "express")
+            {
+                if (weight < 1) costs = 1.8*0.03d*weight;
+                else if (weight >= 1 && weight <= 10) costs = 1.4*0.05d * weight;
+                else if (weight >= 11 && weight <= 40) costs =  1.05*0.1d*weight;
+                else if (weight >= 41 && weight <= 90) costs = 0.02*0.15d*weight + 0.15d;
+                else if (weight >= 91 && weight <= 150) costs = 1.01*0.2d * weight;
+            }
+            costs *= distance;
+            Console.WriteLine("The delivery of your shipment with weight of {0:f3} kg. would cost {1:f2} lv.",
+               weight,costs );
+        }
+    }
+}
