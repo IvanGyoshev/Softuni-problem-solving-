ec40fe5013e31c8a72881c83ee109ea636478ee0
IvanGyoshev <ivan_gy6ev@abv.bg>
2018-07-20 10:08:18 +0300

    Programming Basics Exam - 05 November 2017

--- Exam - 05 November 2017/05.Java/Program.cs
+++ Exam - 05 November 2017/05.Java/Program.cs
@@ -11,15 +11,35 @@ namespace _05.Java
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            //top
             for (int i=1; i<=n; i++)
             Console.WriteLine("{0}~ ~ ~ {1}", new string(' ', n), new string(' ', 2 * n));
-
+            //1-line
             Console.WriteLine("{0} ", new string('=', 3*n+5));
-
-            //Console.WriteLine("{0} ", new string('=', 3 * n + 5));
-
-            Console.WriteLine("{0}   ", new string('=', 3 * n));
-
+            //cup top first half
+            if (n%2==0) Console.WriteLine("|{0}|{1}| ", new string('~', 2 * n + 4),
+                new string(' ', n - 1));
+            for (int j = 1; j<=(n-3)/2; j++)
+                Console.WriteLine("|{0}|{1}| ", new string('~', 2 * n + 4),
+                new string(' ', n - 1));                
+            //middle of cup JAVA 
+             Console.WriteLine("|{0}JAVA{0}|{1}| ", new string('~', n),
+             new string(' ', n - 1));
+            //cup top first half
+            for (int j = 1; j <= (n - 3) / 2; j++)
+                Console.WriteLine("|{0}|{1}| ", new string('~', 2 * n + 4),
+     new string(' ', n - 1));
+            //2-nd Line
+            Console.WriteLine("{0}   ", new string('=', 3 * n+5));            
+            //bottom lines
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine("{1}\\{0}/{1}{2}",
+                    new string('@', 2 * n + 4 - 2 * i), new string(' ', i),
+                    new string(' ', n));
+            }
+            //last Line
+            Console.WriteLine("{0}   ", new string('=', 2 * n + 6));
         }
     }
 }
---
+++ Exam - 05 November 2017/6.happy figures/Lucky Numbers .cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.happy_figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i=1; i<=9; i++)
+            {
+                for (int j=1; j<=9; j++)
+                {
+                    for (int k = 1; k<=9; k++)
+                    {
+                        for (int m=1; m<=9; m++)
+                        {
+                            //a+b=c+d
+                            if (i+j==k+m)
+                            {
+                                if (n % (i + j) == 0)
+                                    Console.Write("{0}{1}{2}{3} ", 
+                                        i,j,k,m);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
